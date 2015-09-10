ab1d9f9b938f5557afa0065606b4e1572649365a
Austin "Paco" DeArmond <ajdearmond@bsu.edu>
2015-09-10 00:25:30 -0400

    Minor formatting revisions
    Editing the original file in Eclipse caused problems with a mix between
    spaces and tabs that were only visible in other editors/viewers.
    
    The static final variables have also been moved to the top of the class.

--- cs222-revisions/assignment2/CSVRecord.java
+++ cs222-revisions/assignment2/CSVRecord.java
@@ -34,48 +34,50 @@ import java.util.Map.Entry;
  */
 public final class CSVRecord implements Serializable, Iterable<String> {
 	
+	private static final String[] EMPTY_STRING_ARRAY = new String[0];
+	private static final long serialVersionUID = 1L;
+
 	public class Builder {
-		private String[] assertStringArray();
 		private Map<String, Integer> mapping;
 		private String comment;
 		private long recordNumber;
 		private long characterPosition;
-		
+
 		/** The values of the record */
 		public Builder withValues(String[] values) {
 			this.values = assertStringArray(values);
 			return this;
 		}
-		
+
 		private assertStringArray(String[] strings) {
 			if (strings == null) {
 				strings = EMPTY_STRING_ARRAY;
 			}
 			return strings;
 		}
-		
+
 		/**
-		 *  The column name to index mapping.
+		 * The column name to index mapping.
 		 */
 		public Builder withMapping(Map<String, Integer> mapping) {
 			this.mapping = mapping;
 			return this;
 		}
-		
-		/** 
-		 * The accumulated comments (if any) 
+
+		/**
+		 * The accumulated comments (if any)
 		 */
 		public Builder withComment(String comment) {
 			this.comment = comment;
 			return this;
 		}
-		
+
 		/** The record number. */
 		public Builder withRecordNumber(long recordNumber) {
 			this.recordNumber = recordNumber;
 			return this;
 		}
-		
+
 		/**
 		 * The character position of this record.
 		 */
@@ -83,143 +85,143 @@ public final class CSVRecord implements Serializable, Iterable<String> {
 			this.characterPosition = characterPosition;
 			return this;
 		}
-		
+
 		public CSVRecord build() {
 			return new CSVRecord(this);
 		}
 	}
 
-    private static final String[] EMPTY_STRING_ARRAY = new String[0];
-	private static final long serialVersionUID = 1L;
-
 	private final Map<String, Integer> mapping;
-	
-    public final long characterPosition;
-    public final String comment;
-    public final long recordNumber;
-    public final String[] values;
 
-    private CSVRecord(Builder builder) {
-    	mapping = builder.mapping;
-    	characterPosition = builder.characterPosition;
-    	comment = builder.comment;
-    	recordNumber = builder.recordNumber;
-    	values = builder.values;
-    }
-	
-    public String getByEnum(final Enum<?> e) {
-        return get(e.toString());
-    }
+	public final long characterPosition;
+	public final String comment;
+	public final long recordNumber;
+	public final String[] values;
 
-    public String getByColumnIndex(final int index) {
-        return values[index];
-    }
+	private CSVRecord(Builder builder) {
+		mapping = builder.mapping;
+		characterPosition = builder.characterPosition;
+		comment = builder.comment;
+		recordNumber = builder.recordNumber;
+		values = builder.values;
+	}
 
-    /**
-     * Returns a value by name.
-     *
-     * @param name
-     *            the name of the column to be retrieved.
-     * @return the column value, maybe null depending on {@link CSVFormat#getNullString()}.
-     * @throws IllegalStateException
-     *             if no header mapping was provided
-     * @throws IllegalArgumentException
-     *             if {@code name} is not mapped or if the record is inconsistent
-     * @see #isConsistent()
-     * @see CSVFormat#withNullString(String)
-     */
-    public String getByColumnName(final String name) {
-        if (mapping == null) {
-            throw new IllegalStateException(
-                "No header mapping was specified, the record values can't be accessed by name");
-        }
-        final Integer index = mapping.get(name);
-        if (index == null) {
-            throw new IllegalArgumentException(String.format("Mapping for %s not found, expected one of %s", name,
-                mapping.keySet()));
-        }
-        try {
-            return values[index.intValue()];
-        } catch (final ArrayIndexOutOfBoundsException e) {
-            throw new IllegalArgumentException(String.format(
-                "Index for header '%s' is %d but CSVRecord only has %d values!", name, index,
-                Integer.valueOf(values.length)));
-        }
-    }
+	public String getByEnum(final Enum<?> e) {
+		return get(e.toString());
+	}
 
-    /**
-     * Tells whether the record size matches the header size.
-     *
-     * @return true of this record is valid, false if not. Some programs can export files that fail this
-     * test but still produce parsable files.
-     */
-    public boolean isConsistent() {
-        return mapping == null || mapping.size() == values.length;
-    }
+	public String getByColumnIndex(final int index) {
+		return values[index];
+	}
 
-    public boolean isMapped(final String name) {
-        return mapping != null && mapping.containsKey(name);
-    }
+	/**
+	 * Returns a value by name.
+	 *
+	 * @param name
+	 *            the name of the column to be retrieved.
+	 * @return the column value, maybe null depending on
+	 *         {@link CSVFormat#getNullString()}.
+	 * @throws IllegalStateException
+	 *             if no header mapping was provided
+	 * @throws IllegalArgumentException
+	 *             if {@code name} is not mapped or if the record is
+	 *             inconsistent
+	 * @see #isConsistent()
+	 * @see CSVFormat#withNullString(String)
+	 */
+	public String getByColumnName(final String name) {
+		if (mapping == null) {
+			throw new IllegalStateException(
+					"No header mapping was specified, the record values can't be accessed by name");
+		}
+		final Integer index = mapping.get(name);
+		if (index == null) {
+			throw new IllegalArgumentException(
+					String.format("Mapping for %s not found, expected one of %s", name, mapping.keySet()));
+		}
+		try {
+			return values[index.intValue()];
+		} catch (final ArrayIndexOutOfBoundsException e) {
+			throw new IllegalArgumentException(
+					String.format("Index for header '%s' is %d but CSVRecord only has %d values!", name, index,
+							Integer.valueOf(values.length)));
+		}
+	}
 
-    /**
-     * Checks whether a given columns is mapped and has a value.
-     *
-     * @param name
-     *            the name of the column to be retrieved.
-     * @return whether a given columns is mapped and has a value
-     */
-    public boolean isSet(final String name) {
-        return isMapped(name) && mapping.get(name).intValue() < values.length;
-    }
+	/**
+	 * Tells whether the record size matches the header size.
+	 *
+	 * @return true of this record is valid, false if not. Some programs can
+	 *         export files that fail this test but still produce parsable
+	 *         files.
+	 */
+	public boolean isConsistent() {
+		return mapping == null || mapping.size() == values.length;
+	}
 
-    @Override
-    public Iterator<String> iterator() {
-        return toList().iterator();
-    }
+	public boolean isMapped(final String name) {
+		return mapping != null && mapping.containsKey(name);
+	}
 
-    /**
-     * Puts all values of this record into the given Map.
-     *
-     * @param map
-     *            The Map to populate.
-     * @return the given map.
-     */
-    <M extends Map<String, String>> M putIn(final M map) {
-        if (mapping == null) {
-            return map;
-        }
-        for (final Entry<String, Integer> entry : mapping.entrySet()) {
-            final int col = entry.getValue().intValue();
-            if (col < values.length) {
-                map.put(entry.getKey(), values[col]);
-            }
-        }
-        return map;
-    }
+	/**
+	 * Checks whether a given columns is mapped and has a value.
+	 *
+	 * @param name
+	 *            the name of the column to be retrieved.
+	 * @return whether a given columns is mapped and has a value
+	 */
+	public boolean isSet(final String name) {
+		return isMapped(name) && mapping.get(name).intValue() < values.length;
+	}
 
-    public int size() {
-        return values.length;
-    }
+	@Override
+	public Iterator<String> iterator() {
+		return toList().iterator();
+	}
 
-    /**
-     * Copies this record into a new Map. The new map is not connected to the Record object, so making changes to it
-	 * will not affect this record.
-     *
-     * @return A new Map. The map is empty if the record has no headers.
-     */
-    public Map<String, String> toMap() {
-        return putIn(new HashMap<String, String>(values.length));
-    }
+	/**
+	 * Puts all values of this record into the given Map.
+	 *
+	 * @param map
+	 *            The Map to populate.
+	 * @return the given map.
+	 */
+	<M extends Map<String, String>> M putIn(final M map) {
+		if (mapping == null) {
+			return map;
+		}
+		for (final Entry<String, Integer> entry : mapping.entrySet()) {
+			final int col = entry.getValue().intValue();
+			if (col < values.length) {
+				map.put(entry.getKey(), values[col]);
+			}
+		}
+		return map;
+	}
 
-    /**
-     * Returns a string representation of the contents of this record. The result is constructed by comment, mapping,
-     * recordNumber and by passing the internal values array to {@link Arrays#toString(Object[])}.
-     */
-    @Override
-    public String toString() {
-        return "CSVRecord [comment=" + comment + ", mapping=" + mapping +
-                ", recordNumber=" + recordNumber + ", values=" +
-                Arrays.toString(values) + "]";
-    }
+	public int size() {
+		return values.length;
+	}
+
+	/**
+	 * Copies this record into a new Map. The new map is not connected to the
+	 * Record object, so making changes to it will not affect this record.
+	 *
+	 * @return A new Map. The map is empty if the record has no headers.
+	 */
+	public Map<String, String> toMap() {
+		return putIn(new HashMap<String, String>(values.length));
+	}
+
+	/**
+	 * Returns a string representation of the contents of this record. The
+	 * result is constructed by comment, mapping, recordNumber and by passing
+	 * the internal values array to {@link Arrays#toString(Object[])}.
+	 */
+	@Override
+	public String toString() {
+		return "CSVRecord [comment=" + comment + ", mapping=" + mapping + ", recordNumber=" + recordNumber + ", values="
+				+ Arrays.toString(values) + "]";
+	}
 
 }
